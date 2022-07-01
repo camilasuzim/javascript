8bf0b40983016996f1778facc175c55678f21170
Camila Suzim <102181988+camilasuzim@users.noreply.github.com>
2022-07-01 17:12:07 -0300

    Add files via upload

---
+++ javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Camila", 22))
+console.log(mostraCidade("Camila", 'Curitiba'))
+console.log(mostraHobby("Camila", 'ler livros'))
